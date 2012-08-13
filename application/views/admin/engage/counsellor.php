e6cdf5bfd7515e13c005f78a68aeb88a8a120f69
Sumit Munjal <sumitmunjal@webinfomart.com>
2012-08-13 11:24:45 +0530

    changes in  the admin seciton

--- application/views/admin/engage/counsellor.php
+++ application/views/admin/engage/counsellor.php
@@ -10,14 +10,14 @@
 	<header>
 		<div class="span6 float_r margin2">
 			<div class="float_l border_holder">
-				<img src="images/images/images/user.png" alt="User" title="User"/>
+				<img src="<?php echo $base; ?>images/images/user.png" alt="User" title="User"/>
 				<span class="margin_l">Hello Administrator</span>
 			</div>
 			<div class="float_l border_holder">
-				<img src="images/images/images/msg.png" alt="User" title="User"/>
+				<img src="<?php echo $base; ?>images/images/msg.png" alt="User" title="User"/>
 			</div>
 			<div class="float_l border_holder">
-				<img src="images/images/images/setting.png" alt="User" title="User"/>
+				<img src="<?php echo $base; ?>images/images/setting.png" alt="User" title="User"/>
 			</div>
 			<div class="float_l border_holder border_beta">
 				logout
@@ -26,26 +26,26 @@
 	</header>
 	<div class="float_l sidebar">
 		<div class="search_box">
-			<img src="images/images/images/search_icon.png" class="search_input_set"><input type="text" name="search" value="search" class="search_input">
+			<img src="<?php echo $base; ?>images/images/search_icon.png" class="search_input_set"><input type="text" name="search" value="search" class="search_input">
 		</div>
 		<div class="menu">
 			<ul id="nav">
-                <li class="active"><a href="#"><img src="images/images/images/home_icon.png" /> <span>Dashboard</span></a></li>
-                <li><a href="#"><img src="images/images/images/paged_icon.png" /><span>Pages</span></a>
+                <li class="active"><a href="#"><img src="<?php echo $base; ?>images/images/home_icon.png" /> <span>Dashboard</span></a></li>
+                <li><a href="#"><img src="<?php echo $base; ?>images/images/paged_icon.png" /><span>Pages</span></a>
                 </li>
-               <li><a href="#"><img src="images/images/images/side_user_icon.png" /><span>Users</span></a>
+               <li><a href="#"><img src="<?php echo $base; ?>images/images/side_user_icon.png" /><span>Users</span></a>
                 </li>
-                <li><a href="#"><img src="images/images/images/edit_icon.png" /><span>Articles</span></a></li>
-                <li><a href="#"><img src="images/images/images/cal_icon.png" /><span>Events</span></a></li>
-                <li><a href="#"><img src="images/images/images/lib_icon.png" /><span>Manage Universities</span></a></li> 
-				<li><a href="#"><img src="images/images/images/img_icon.png" /><span>Manage Home Gallery</span></a></li> 
-				<li><a href="#"><img src="images/images/images/book_icon.png" /><span>Proagram/Courses</span></a></li> 
-				<li><a href="#"><img src="images/images/images/book_icon.png" /><span>University/Courses</span></a></li>
-				<li><a href="#"><img src="images/images/images/img_icon.png" /><span>University Gallery</span></a></li>
-				<li><a href="#"><img src="images/images/images/clip_icon.png" /><span>Manage Univ/Proagram</span> </a></li>
-				<li><a href="#"><img src="images/images/images/clip_icon.png" /><span>Manage Univ/Users</span> </a></li>
-				<li><a href="#"><img src="images/images/images/setting.png" /><span>Settings</span> </a></li>
-				<li><a href="#"><img src="images/images/images/bulb_icon.png" /><span>Supports</span></a></li>
+                <li><a href="#"><img src="<?php echo $base; ?>images/images/edit_icon.png" /><span>Articles</span></a></li>
+                <li><a href="#"><img src="<?php echo $base; ?>images/images/cal_icon.png" /><span>Events</span></a></li>
+                <li><a href="#"><img src="<?php echo $base; ?>images/images/lib_icon.png" /><span>Manage Universities</span></a></li> 
+				<li><a href="#"><img src="<?php echo $base; ?>images/images/img_icon.png" /><span>Manage Home Gallery</span></a></li> 
+				<li><a href="#"><img src="<?php echo $base; ?>images/images/book_icon.png" /><span>Proagram/Courses</span></a></li> 
+				<li><a href="#"><img src="<?php echo $base; ?>images/images/book_icon.png" /><span>University/Courses</span></a></li>
+				<li><a href="#"><img src="<?php echo $base; ?>images/images/img_icon.png" /><span>University Gallery</span></a></li>
+				<li><a href="#"><img src="<?php echo $base; ?>images/images/clip_icon.png" /><span>Manage Univ/Proagram</span> </a></li>
+				<li><a href="#"><img src="<?php echo $base; ?>images/images/clip_icon.png" /><span>Manage Univ/Users</span> </a></li>
+				<li><a href="#"><img src="<?php echo $base; ?>images/images/setting.png" /><span>Settings</span> </a></li>
+				<li><a href="#"><img src="<?php echo $base; ?>images/images/bulb_icon.png" /><span>Supports</span></a></li>
             </ul>
 		</div>
 	</div>
