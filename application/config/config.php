1a31194bf971c016902953439c56f71d836b2eb0
Sumit Munjal <sumitmunjal@webinfomart.com>
2012-08-13 11:26:59 +0530

    changes in telecaller page

--- application/config/config.php
+++ application/config/config.php
@@ -267,7 +267,7 @@ $config['sess_time_to_update']	= 300;
 |
 */
 $config['cookie_prefix']	= "";
-$config['cookie_domain']	= ".meetuniversities.info/";
+$config['cookie_domain']	= ".meetuniversities.info";
 $config['cookie_path']		= "/";
 $config['cookie_secure']	= FALSE;
 
@@ -365,7 +365,7 @@ $config['img_path'] = 'images';
 $config['admin_css_path'] ='css/admin';
 $config['admin_img']='images/admin';
 $config['js']='js';
-$config['domain']='.meetuniversities.info/';
+$config['domain']='.meetuniversities.info';
 $config['mailtype'] = 'html';
 //$config['protocol'] = 'sendmail';
 $config['protocol'] = 'mail';
