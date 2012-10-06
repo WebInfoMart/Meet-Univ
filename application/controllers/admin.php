34f06251c9f1e096432bab06f60c784ed2c70194
Sumit Munjal <sumitmunjal@webinfomart.com>
2012-10-06 16:42:28 +0530

    changes in header and footer page

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -18,7 +18,7 @@ class Admin extends CI_Controller
 		$this->load->library('tank_auth');
 		$this->lang->load('tank_auth');
 		$this->load->library('googleanalytics');
-		$this->session->set_userdata('chat_username','Counsellor_break_1212111_12121'); // Must be already set
+		//$this->session->set_userdata('chat_username','Counsellor_break_1212111_12121'); // Must be already set
    
 		
 	}
